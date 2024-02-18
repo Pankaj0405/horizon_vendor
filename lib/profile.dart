dc06ea1ea131b3dd288c9c027fda6e44bb178367
Tushar Singh <101038096+tushars2610@users.noreply.github.com>
2024-02-18 08:17:55 +0530

    second commit

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:horizon_vendor/Controllers/auth_controller1.dart';
+import 'package:horizon_vendor/Widgets/text_fields.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -11,7 +12,17 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   final auth = Get.put(AuthController1());
+  final TextEditingController _textEditingController = TextEditingController();
   String guestName = "Guest";
+  void changeName() {
+    setState(() {
+      guestName = _textEditingController.text;
+    });
+    setState(() {
+      navigator?.pop(context);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,21 +50,31 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Column(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text(
-                  "Hii!",
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-                const SizedBox(
-                  width: 30,
-                ),
-                Text(
-                  guestName,
-                  style: const TextStyle(
-                    fontSize: 30,
-                  ),
+                Row(
+                  children: [
+                    const Text(
+                      "Hii!",
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    SizedBox(
+                      width: 200,
+                      child: Text(
+                        guestName,
+                        style: const TextStyle(
+                          fontSize: 30,
+                        ),
+                        overflow: TextOverflow.fade,
+                        maxLines: 2,
+                      ),
+                    ),
+                  ],
                 ),
                 IconButton(
                   onPressed: () {
@@ -61,7 +82,48 @@ class _ProfilePageState extends State<ProfilePage> {
                       context: context,
                       builder: ((context) {
                         return Container(
-                          height: 100,
+                          height: 600,
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 7, 159, 159)
+                                .withOpacity(0.5),
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(25),
+                              topRight: Radius.circular(25),
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.all(10),
+                                padding: const EdgeInsets.only(top: 20),
+                                child: textField(
+                                  "Enter your name",
+                                  _textEditingController,
+                                  TextInputType.text,
+                                ),
+                              ),
+                              const SizedBox(height: 30),
+                              TextButton(
+                                onPressed: changeName,
+                                style: const ButtonStyle(
+                                  elevation: MaterialStatePropertyAll(10),
+                                  backgroundColor: MaterialStatePropertyAll(
+                                    Color.fromARGB(255, 7, 159, 159),
+                                  ),
+                                  padding: MaterialStatePropertyAll(
+                                    EdgeInsets.only(left: 20, right: 20),
+                                  ),
+                                ),
+                                child: const Text(
+                                  "Save",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         );
                       }),
                     );
