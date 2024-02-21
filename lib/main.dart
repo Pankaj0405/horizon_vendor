5d9a7d7466b3d20f69213684e7ed5e19376f24ff
Pankaj <pbatra0426@gmail.com>
2024-02-21 18:49:57 +0530

    main change

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:horizon_vendor/splash_Screen.dart';
@@ -22,9 +23,15 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    return  ScreenUtilInit(
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_ , child){
+       return GetMaterialApp(
+         debugShowCheckedModeBanner: false,
+         home: SplashScreen(),
+       );
+      },
     );
   }
 }
